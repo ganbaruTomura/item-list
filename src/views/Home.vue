7b049210381c95e40f134ce668948c44c716a2cc
ganbaruTomura <tomuraganbaru@gmail.com>
2022-03-22 23:15:58 +0900

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ state.items }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      items: {},
+    });
+
+    // API
+    async function getData() {
+        await axios.get("http://localhost:3000/items").then(function (response) {
+    state.items = response.data
+        console.log(response.data)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    }
+
+    getData();
+
+    return { state };
+  },
+});
+</script>
